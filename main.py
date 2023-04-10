1ef7801a5ea5bf086c647bec59348f76bae20718
Niewiaro <kubuskrol@gmail.com>
2023-04-10 23:20:25 +0200

    event handeling - closing

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ def main():
     print(pygame.__version__)
 
     screen = pygame.display.set_mode(size=(1280, 720))
-    
+
+    while True:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
 
 
 if __name__ == '__main__':
     main()
+    
